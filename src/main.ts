8735e1d218777af98b5205fffb9f5904c451f626
jay-flow <kimpetro153@gmail.com>
2021-05-03 10:23:40 +0900

    [Feature] Install Vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import "./styles/tailwind.css"
+import { routes } from "./routes"
+import { createRouter, createWebHistory } from "vue-router"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+app.use(router).mount("#app")
