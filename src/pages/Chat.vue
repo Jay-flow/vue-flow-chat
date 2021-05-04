76c5387fed00ecb0a317865f62e3b3e8ad5cc40d
jay-flow <kimpetro153@gmail.com>
2021-05-04 15:15:55 +0900

    [Feature] Add Key board notification

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -7,11 +7,36 @@
       />
       <span class="text-white text-xl">Chat</span>
     </div>
-    <div class="flex-grow flex flex-col-reverse bg-teal-400">11</div>
+    <div class="flex-grow flex flex-col-reverse bg-teal-400 p-3 relative">
+      <fade
+        v-show="isNotifyKeyInfo"
+        class="absolute top-0 left-0 text-center w-full"
+      >
+        <span class="text-xs text-gray-900">
+          입력 중 Shift + Enter를 누르시면 메세지를 상대방에게 바로 전송할 수
+          있습니다.
+        </span>
+      </fade>
+      <div class="flex justify-end items-end">
+        <span class="mr-2 text-gray-900">11:30</span>
+        <div class="bg-white px-3 py-1 rounded-full text-gray-900">
+          Hello world!
+        </div>
+      </div>
+      <div class="flex justify-start items-end">
+        <div class="bg-white px-3 py-1 rounded-full text-gray-900">
+          How are you?
+        </div>
+        <span class="ml-2 text-gray-900">11:30</span>
+      </div>
+    </div>
     <div class="flex border-t p-3">
       <div
+        ref="inputBox"
         contenteditable="true"
+        autofocus
         class="flex-grow h-auto max-h-40 overflow-auto focus-visible:outline-none text-xl"
+        @keydown.shift.enter.prevent="send"
       ></div>
       <div
         class="flex items-end pb-0.5 w-10 h-full px-2 cursor-pointer"
@@ -27,22 +52,39 @@
   import { defineComponent } from "vue"
   import LeftArrowIcon from "../components/svg/LeftArrowIcon.vue"
   import SendIcon from "../components/svg/SendIcon.vue"
+  import Fade from "../components/animations/Fade.vue"
 
   export default defineComponent({
     name: "Chat",
     components: {
       LeftArrowIcon,
       SendIcon,
+      Fade,
     },
     data() {
-      return {}
+      return {
+        isNotifyKeyInfo: true,
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.isNotifyKeyInfo = false
+      }, 5000)
     },
     methods: {
       backButtonClick() {
         this.$router.back()
       },
+      cleanUpInput() {
+        this.$refs.inputBox.innerText = ""
+        this.$refs.inputBox.focus()
+      },
       send() {
-        alert("send")
+        const sendText = this.$refs.inputBox.innerText
+        this.cleanUpInput()
+      },
+      pressKeyHandler(event) {
+        console.log(event)
       },
     },
   })
